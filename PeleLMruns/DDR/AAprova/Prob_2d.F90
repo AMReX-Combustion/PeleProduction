fa99614d8003990ffbec5f134b4ea8d143acd9f8
Marc Day <MSDay@lbl.gov>
2019-06-24 19:34:40 -0700

    First cut of DDR add, not functioning yet.

---
+++ PeleLMruns/DDR/AAprova/Prob_2d.F90
@@ -0,0 +1,413 @@
+#include <AMReX_REAL.H>
+#include <AMReX_CONSTANTS.H>
+#include <AMReX_BC_TYPES.H>
+#include <AMReX_ArrayLim.H>
+
+#include <Prob_F.H>
+#include <PeleLM_F.H>
+
+module prob_2D_module
+
+  use fuego_chemistry
+
+  implicit none
+
+  private
+  
+  public :: amrex_probinit, setupbc, init_data, getZone
+
+contains
+
+! ::: -----------------------------------------------------------
+! ::: This routine is called at problem initialization time
+! ::: and when restarting from a checkpoint file.
+! ::: The purpose is (1) to specify the initial time value
+! ::: (not all problems start at time=0.0) and (2) to read
+! ::: problem specific data from a namelist or other input
+! ::: files and possibly store them or derived information
+! ::: in FORTRAN common blocks for later use.
+! ::: 
+! ::: 
+! ::: INPUTS/OUTPUTS:
+! ::: 
+! ::: init      => TRUE if called at start of problem run
+! :::              FALSE if called from restart
+! ::: strttime <=  start problem with this time variable
+! ::: 
+! ::: -----------------------------------------------------------
+  subroutine amrex_probinit (init,name,namlen,problo,probhi) bind(c, name='amrex_probinit')
+
+    use PeleLM_F,  only: pphys_getP1atm_MKS
+    use mod_Fvar_def, only : pamb, dpdt_factor, closed_chamber
+    use mod_Fvar_def, only : fuelID, domnhi, domnlo, dim
+    use probdata_module, only : rho_bc, Y_bc
+    use probdata_module, only : T_fu, T_ox, T_air, pipeTh, pipeBL,&
+         V_fu, V_ox, V_air, fuel_ox_split, ox_air_split,blobx, bloby, blobr, blobT, blobw
+
+    implicit none
+
+    integer init, namlen
+    integer name(namlen)
+    integer untin
+    REAL_T problo(dim), probhi(dim)
+
+    integer i, istemp
+    REAL_T FORT_P1ATMMKS, area
+
+    namelist /fortin/ T_fu, T_ox, T_air, pipeTh, pipeBL,&
+                      V_fu, V_ox, V_air, fuel_ox_split, ox_air_split,&
+                      blobx, bloby, blobr, blobT, blobw
+    namelist /heattransin/ pamb
+
+!
+!      Build 'probin' filename -- the name of file containing fortin namelist.
+!
+    integer maxlen, isioproc
+    parameter (maxlen=256)
+    character probin*(maxlen)
+
+    call bl_pd_is_ioproc(isioproc)
+
+    if (namlen .gt. maxlen) then
+       call bl_pd_abort('probin file name too long')
+    end if
+
+    if (namlen .eq. 0) then
+       namlen = 6
+       probin(1:namlen) = 'probin'
+    else
+       do i = 1, namlen
+          probin(i:i) = char(name(i))
+       end do
+    endif
+
+!     Load domain dimensions into common (put something computable there for SDIM<3)
+    domnlo(:) = 0.d0
+    domnhi(:) = 0.d0
+
+    domnlo(1:dim) = problo(1:dim)
+    domnhi(1:dim) = probhi(1:dim)
+
+    untin = 9
+    open(untin,file=probin(1:namlen),form='formatted',status='old')
+      
+    ! Set defaults
+    pamb = pphys_getP1atm_MKS()
+
+    blobr = 0.d0
+    blobx = 0.5d0 * (domnhi(1) + domnlo(1))
+    bloby = 0.5d0 * (domnhi(2) + domnlo(2))
+    blobw = 1.d0
+    fuel_ox_split = .001d0
+    ox_air_split =  .0125d0
+    pipeTh = 0.d0
+    pipeBL = 1.d0
+    V_fu = -0.2d0
+    V_ox = 0.2d0
+    V_air = 0.2d0
+    T_fu = 300.d0
+    T_ox = 300.d0
+    T_air = 300.d0
+
+    ! Note: for setup with no coflow, set Ro=Rf+wallth
+
+    if (isioproc .eq. 1) write(6,*)"reading fortin"
+    read(untin,fortin)
+    if (isioproc .eq. 1) write(6,*)"done reading fortin"
+
+    read(untin,heattransin)
+ 
+!     Set up boundary functions
+    if (isioproc .eq. 1) write(6,*)" setup bc"
+    call setupbc()
+    if (isioproc .eq. 1) write(6,*)" done setup bc"
+
+    area = 1.d0
+    do i=1,dim-1
+       area = area*(domnhi(i)-domnlo(i))
+    enddo
+
+    if (isioproc.eq.1) then
+       write(6,fortin)
+       write(6,heattransin)
+#if defined(BL_DO_FLCT)
+       write(6,flctin)
+#endif
+    end if
+
+  end subroutine amrex_probinit
+
+  subroutine setupbc() bind(c, name='setupbc')
+      
+    use network,   only: nspec
+    use PeleLM_F, only: pphys_getP1atm_MKS
+    use PeleLM_2D, only: pphys_RHOfromPTY, pphys_HMIXfromTY
+    use mod_Fvar_def, only : pamb, domnlo, maxspec, maxspnml, dim
+    use probdata_module, only : Y_bc, T_bc, u_bc, v_bc, rho_bc, h_bc
+    use probdata_module, only : bcinit, iN2, iO2, iCO2, iCH4, iH2O, &
+         BL_FUELPIPE, BL_OUTFLOW, BL_OXIDIZER, BL_AIR, BL_PIPEEND, BL_VOLUME,&
+         T_air, T_ox, T_fu, V_air, V_fu, V_ox
+    implicit none
+    
+    REAL_T Patm, pmf_vals(maxspec+3), a
+    REAL_T Xt(maxspec), Yt(maxspec), loc
+    REAL_T rho	! added
+    integer zone, n, getZone, fuelZone, airZone, oxZone, volZone, peZone, ofZone, region, len
+    integer b(dim)
+    integer num_zones_defined
+    character*(maxspnml) name
+
+    b = 1
+    Patm = pamb / pphys_getP1atm_MKS()
+
+    do n=1,Nspec
+       call get_spec_name(name,n)
+       if (name .eq. 'N2' ) iN2 = n
+       if (name .eq. 'O2' ) iO2 = n
+       if (name .eq. 'CO2' ) iCO2 = n
+       if (name .eq. 'CH4' ) iCH4 = n
+       if (name .eq. 'H2O' ) iH2O = n
+    enddo
+
+    !     A diffusion flame
+
+    fuelZone = BL_FUELPIPE
+    oxZone   = BL_OXIDIZER
+    airZone  = BL_AIR
+    volZone  = BL_VOLUME
+    peZone   = BL_PIPEEND
+    ofZone   = BL_OUTFLOW
+    num_zones_defined = 6
+
+    !     Fuel
+    do n = 1,Nspec
+       Yt(n) = 0.d0
+    end do
+
+    Yt(iCH4) = 1.0d0
+    Yt(iO2) = 0.d0
+    Yt(iH2O) = 0.d0
+    Yt(iN2) = 1.d0 -  Yt(iCH4) -  Yt(iO2) - Yt(iH2O)
+
+    do n=1,Nspec
+       Y_bc(n-1,fuelZone) = Yt(n)
+    end do
+    T_bc(fuelZone) = T_fu
+    u_bc(fuelZone) = 0.d0
+    v_bc(fuelZone) = V_fu
+
+    !     Oxidizer
+    do n = 1,Nspec
+       Yt(n) = 0.d0
+    end do
+
+    Yt(iO2) = 0.2395d0
+    Yt(iCO2) = 0.000d0
+    Yt(iN2) = 1.d0 -  Yt(iO2) -  Yt(iCO2)
+       
+    do n=1,Nspec
+       Y_bc(n-1,oxZone) = Yt(n)
+    end do
+    T_bc(oxZone) = T_ox
+    u_bc(oxZone) = 0.d0
+    v_bc(oxZone) = V_ox
+
+    !     Air
+    do n=1,Nspec
+       Xt(n) = zero
+    enddo
+    Xt(iO2) = 0.21d0
+    Xt(iN2) = 1.d0 - Xt(iO2)
+
+    CALL CKXTY (Xt, Yt)         
+    do n=1,Nspec
+       Y_bc(n-1,airZone) = Yt(n)
+    end do
+       
+    T_bc(airZone) = T_air
+    u_bc(airZone) = 0.d0
+    v_bc(airZone) = V_air
+
+    !     Pipeend (as oxidizer
+    do n=1,Nspec
+       Y_bc(n-1,peZone) = Y_bc(n-1,oxZone)
+    end do
+         
+    T_bc(peZone) = T_bc(oxZone)
+    u_bc(peZone) = u_bc(oxZone)
+    v_bc(peZone) = 0.d0
+
+    !     Volume (as air)
+    do n=1,Nspec
+       Y_bc(n-1,volZone) = Y_bc(n-1,airZone)
+    end do
+
+    T_bc(volZone) = T_bc(airZone)
+    u_bc(volZone) = u_bc(airZone)
+    v_bc(volZone) = v_bc(airZone)
+
+    !     Outflow (as air, except phi)
+    do n=1,Nspec
+       Y_bc(n-1,ofZone) = Y_bc(n-1,airZone)
+    end do
+    
+    T_bc(ofZone) = T_bc(airZone)
+    u_bc(ofZone) = u_bc(airZone)
+    v_bc(ofZone) = v_bc(airZone)
+
+    do zone=1,num_zones_defined
+
+!     Set density and hmix consistent with data
+
+       call pphys_RHOfromPTY(b, b, &
+                             rho_bc(zone), DIMARG(b), DIMARG(b),&
+                             T_bc(zone),   DIMARG(b), DIMARG(b),&
+                             Y_bc(0,zone), DIMARG(b), DIMARG(b), Patm)
+
+       call pphys_HMIXfromTY(b, b, &
+                             h_bc(zone),   DIMARG(b), DIMARG(b),&
+                             T_bc(zone),   DIMARG(b), DIMARG(b),&
+                             Y_bc(0,zone), DIMARG(b), DIMARG(b))
+
+    enddo
+
+    bcinit = .true.
+
+  end subroutine setupbc
+
+
+! ::: -----------------------------------------------------------
+! ::: This routine is called at problem setup time and is used
+! ::: to initialize data on each grid.  The velocity field you
+! ::: provide does not have to be divergence free and the pressure
+! ::: field need not be set.  A subsequent projection iteration
+! ::: will define aa divergence free velocity field along with a
+! ::: consistant pressure.
+! ::: 
+! ::: NOTE:  all arrays have one cell of ghost zones surrounding
+! :::        the grid interior.  Values in these cells need not
+! :::        be set here.
+! ::: 
+! ::: INPUTS/OUTPUTS:
+! ::: 
+! ::: level     => amr level of grid
+! ::: time      => time at which to init data             
+! ::: lo,hi     => index limits of grid interior (cell centered)
+! ::: nscal     => number of scalar quantities.  You should know
+! :::		   this already!
+! ::: vel      <=  Velocity array
+! ::: scal     <=  Scalar array
+! ::: press    <=  Pressure array
+! ::: delta     => cell size
+! ::: xlo,xhi   => physical locations of lower left and upper
+! :::              right hand corner of grid.  (does not include
+! :::		   ghost region).
+! ::: -----------------------------------------------------------
+  subroutine init_data(level,time,lo,hi,nscal,&
+                       vel,scal,DIMS(state),press,DIMS(press),&
+                       delta,xlo,xhi) bind(c, name='init_data')
+    use network,   only: nspec
+    use PeleLM_F,  only: pphys_getP1atm_MKS, pphys_get_spec_name2
+    use PeleLM_2D, only: pphys_RHOfromPTY, pphys_HMIXfromTY
+    use mod_Fvar_def, only : Density, Temp, FirstSpec, RhoH, pamb, Trac, dim
+    use mod_Fvar_def, only : bathID, domnhi, domnlo, maxspec, maxspnml
+    use probdata_module, only : BL_XLO, blobr, blobT, blobw, blobx, bloby
+    use probdata_module, only : bcinit, iN2, iO2, iCO2, iCH4, iH2O, &
+         BL_FUELPIPE, BL_OUTFLOW, BL_OXIDIZER, BL_AIR, BL_PIPEEND, BL_VOLUME
+
+    implicit none
+
+    integer    level, nscal
+    integer    lo(dim), hi(dim)
+    integer    DIMDEC(state)
+    integer    DIMDEC(press)
+    REAL_T     xlo(dim), xhi(dim)
+    REAL_T     time, delta(dim)
+    REAL_T     vel(DIMV(state),dim)
+    REAL_T    scal(DIMV(state),nscal)
+    REAL_T   press(DIMV(press))
+    integer tmpi, nPMF
+
+    integer i, j, n, airZone, fuelZone, getZone, zone, len
+    REAL_T x, y, r, Yl(maxspec), Xl(maxspec), Patm
+    REAL_T pmf_vals(maxspec+3), y1, y2, dx
+    REAL_T pert,Lx,FORT_P1ATMMKS,eta,u,v,rho,T,h
+    REAL_T phiV
+
+    if (iN2.lt.1 .or. iN2.gt.Nspec) then
+       call bl_pd_abort('N2 id not sest prior to calling INITDATA')
+    endif
+    
+    do j = lo(2), hi(2)
+       y = (float(j)+.5)*delta(2)+domnlo(2)
+       do i = lo(1), hi(1)
+          x = (float(i)+.5)*delta(1)+domnlo(1)
+
+          call bcfunction(BL_XLO,x,y,time,u,v,rho,Yl,T,phiV,h,delta,.true.)
+
+          do n=1,Nspec
+             scal(i,j,FirstSpec-1+n) = Yl(n)
+          enddo
+          scal(i,j,Temp) = T
+
+          if (blobr.gt.0.d0) then
+             r = SQRT((x-blobx)**2 + (y-bloby)**2)
+             eta = 0.5d0*(1.d0 - TANH(2.d0*(r-blobr)/blobw))
+             scal(i,j,Temp) = blobT*eta + scal(i,j,Temp)*(1.d0-eta)
+          endif
+
+          vel(i,j,1) = u
+          vel(i,j,2) = v
+          scal(i,j,Trac) = 0.d0
+
+       enddo
+    enddo
+
+    Patm = pamb / pphys_getP1atm_MKS()
+
+    call pphys_RHOfromPTY(lo,hi,&
+                          scal(ARG_L1(state),ARG_L2(state),Density),  DIMS(state),&
+                          scal(ARG_L1(state),ARG_L2(state),Temp),     DIMS(state),&
+                          scal(ARG_L1(state),ARG_L2(state),FirstSpec),DIMS(state),&
+                          Patm)
+
+    call pphys_HMIXfromTY(lo,hi,&
+                          scal(ARG_L1(state),ARG_L2(state),RhoH),     DIMS(state),&
+                          scal(ARG_L1(state),ARG_L2(state),Temp),     DIMS(state),&
+                          scal(ARG_L1(state),ARG_L2(state),FirstSpec),DIMS(state))
+
+    do j = lo(2), hi(2)
+       do i = lo(1), hi(1)
+          do n = 0,Nspec-1
+             scal(i,j,FirstSpec+n) = scal(i,j,FirstSpec+n)*scal(i,j,Density)
+          enddo
+          scal(i,j,RhoH) = scal(i,j,RhoH)*scal(i,j,Density)
+       enddo
+    enddo
+  end subroutine init_data
+
+  integer function getZone(x, y)
+    
+    use mod_Fvar_def, only : domnhi, domnlo, dim
+    use probdata_module, only : BL_FUELPIPE, BL_OUTFLOW, BL_OXIDIZER, BL_AIR, BL_PIPEEND, BL_VOLUME,&
+         fuel_ox_split, ox_air_split, pipeTh
+
+    implicit none
+    REAL_T x, y
+    getZone = BL_VOLUME
+    if (y.le.domnlo(2)) then
+       if (ABS(x).le.fuel_ox_split) then
+          getZone = BL_FUELPIPE
+       else if (ABS(x).le.fuel_ox_split+pipeTh) then
+          getZone = BL_PIPEEND
+       else if (ABS(x).le.ox_air_split) then
+          getZone = BL_OXIDIZER
+       else
+          getZone = BL_AIR
+       endif
+    else if (y.ge.domnhi(2)) then
+       getZone = BL_OUTFLOW
+    endif
+  end function getZone
+      
+end module prob_2D_module
---
+++ PeleLMruns/DDR/AAprova/user_defined_fcts_2d.F90
@@ -0,0 +1,205 @@
+#include <AMReX_REAL.H>
+#include <AMReX_CONSTANTS.H>
+#include <AMReX_BC_TYPES.H>
+#include <PeleLM_F.H>
+#include <AMReX_ArrayLim.H>
+
+module user_defined_fcts_2d_module
+
+  implicit none
+  
+  private
+  
+  public :: bcfunction, zero_visc
+
+contains
+
+  subroutine bcfunction(RegionID,x,y,time,u,v,rho,Yl,T,h,dx,getuv) bind(c, name='bcfunction')
+
+    use network,   only: Nspec
+    use mod_Fvar_def, only : maxspec, pamb
+    use probdata_module, only : bcinit, rho_bc, Y_bc, T_bc, h_bc, v_bc, &
+         BL_FUELPIPE, BL_OUTFLOW, BL_OXIDIZER, BL_AIR, BL_PIPEEND, BL_VOLUME,&
+         fuel_ox_split, blobw, bloby, pipeBL, pipeTh
+    use prob_2D_module, only : getZone
+      
+    implicit none
+
+    integer RegionID
+    REAL_T x, y, time, u, v, rho, Yl(0:*), T, h, dx(2), r
+    logical getuv
+    integer b(2)
+
+    integer n, zone
+    REAL_T eta, xmid, Patm, FORT_P1ATMMKS
+         
+    REAL_T Wf, Wa, Wm, mf, Yf
+
+    REAL_T,  parameter :: HtoTerrMAX = BL_REAL_E(7.8,-12)
+    integer, parameter :: HtoTiterMAX = 20
+    REAL_T res(0:HtoTiterMAX-1)
+    integer Niter
+    integer zoneL, zoneR, zoneT
+    character*(maxspnml) name
+
+    data  b / 1, 1 /
+    
+    if (.not. bcinit) then
+       call bl_abort('Need to initialize boundary condition function')
+    end if
+
+    zone = getZone(x,y)
+    if (zone .eq. BL_OUTFLOW) then
+       rho = rho_bc(zone)
+       do n = 0, Nspec-1
+          Yl(n) = Y_bc(n,zone)
+       end do
+       T = T_bc(zone)
+       h = h_bc(zone)
+       if (getuv .eqv. .TRUE.) then
+          u = zero
+          v = v_bc(zone)
+       endif
+    else
+       zoneL = BL_FUELPIPE
+       zoneR = BL_OXIDIZER
+       zoneT = BL_VOLUME         
+       eta = 0.5d0*(1.d0 - TANH(2.d0*(ABS(x)-fuel_ox_split)/blobw))
+       do n = 0, Nspec-1
+          Yl(n) = Y_bc(n,zoneL)*eta + Y_bc(n,zoneR)*(1.d0-eta)
+       end do
+       T = T_bc(zoneL)*eta + T_bc(zoneR)*(1.d0-eta)
+
+       eta = 0.5d0*(1.d0 - TANH(2.d0*(y-bloby)/blobw))
+       do n = 0, Nspec-1
+          Yl(n) = Yl(n)*eta + Y_bc(n,zoneT)*(1.d0-eta)
+       end do
+       T = T*eta + T_bc(zoneT)*(1.d0-eta)
+
+       Patm = pamb / FORT_P1ATMMKS()
+
+       call pphys_RHOfromPTY(b, b, &
+                             rho, DIMARG(b), DIMARG(b),&
+                             T,   DIMARG(b), DIMARG(b),&
+                             Yl,  DIMARG(b), DIMARG(b), Patm)
+          
+       call pphys_HMIXfromTY(b, b, &
+                             h,   DIMARG(b), DIMARG(b),&
+                             T,   DIMARG(b), DIMARG(b),&
+                             Yl,  DIMARG(b), DIMARG(b))
+          
+       if (getuv .eqv. .TRUE.) then
+          u = zero
+          zone = getZone(x,y)
+          if (zone.eq.BL_FUELPIPE) then
+             zoneL = BL_PIPEEND
+             zoneR = BL_FUELPIPE
+             r = fuel_ox_split - ABS(x)
+             eta = 0.5d0*(1.d0 - TANH(2.d0*r/pipeBL))
+             v = v_bc(zoneL)*eta + v_bc(zoneR)*(1.d0-eta)
+             if (eta.lt.0.d0 .or. eta.gt.1.d0) then
+                call bl_pd_abort()
+             endif
+          else if (zone.eq.BL_OXIDIZER) then
+             zoneL = BL_PIPEEND
+             zoneR = BL_OXIDIZER
+             r = ABS(x) - (fuel_ox_split+pipeTh)
+             eta = 0.5d0*(1.d0 - TANH(2.d0*r/pipeBL))
+             v = v_bc(zoneL)*eta + v_bc(zoneR)*(1.d0-eta)
+             if (eta.lt.0.d0 .or. eta.gt.1.d0) then
+                call bl_pd_abort()
+             endif
+          else
+             v = v_bc(zone)
+          endif
+       endif
+    endif
+  end subroutine bcfunction
+
+! ::: -----------------------------------------------------------
+! ::: This routine will zero out diffusivity on portions of the
+! ::: boundary that are inflow, allowing that a "wall" block
+! ::: the complement aperture
+! :::
+! ::: INPUTS/OUTPUTS:
+! :::
+! ::: diff      <=> diffusivity on edges
+! ::: DIMS(diff) => index extent of diff array
+! ::: lo,hi      => region of interest, edge-based
+! ::: domlo,hi   => index extent of problem domain, edge-based
+! ::: dx         => cell spacing
+! ::: problo     => phys loc of lower left corner of prob domain
+! ::: bc         => boundary condition flag (on orient)
+! :::                   in BC_TYPES::physicalBndryTypes
+! ::: idir       => which face, 0=x, 1=y
+! ::: isrz       => 1 if problem is r-z
+! ::: id         => index of state, 0=u
+! ::: ncomp      => components to modify
+! :::
+! ::: -----------------------------------------------------------
+
+  subroutine zero_visc(diff,DIMS(diff),lo,hi,domlo,domhi, &
+                           dx,problo,bc,idir,isrz,id,ncomp) &
+                           bind(C, name="zero_visc")   
+
+    use mod_Fvar_def, only : Density, Temp, FirstSpec, RhoH, LastSpec, dim
+    use probdata_module, only : BL_PIPEEND
+    use prob_2D_module, only : getZone
+      
+    implicit none
+    integer DIMDEC(diff)
+    integer lo(dim), hi(dim)
+    integer domlo(dim), domhi(dim)
+    integer bc(2*dim)
+    integer idir, isrz, id, ncomp
+    REAL_T  diff(DIMV(diff),*)
+    REAL_T  dx(dim)
+    REAL_T  problo(dim)
+
+    integer i, j, n, Tid, RHid, YSid, YEid, ys, ye
+    logical do_T, do_RH, do_Y
+    REAL_T xl, xr, xh, y
+
+    Tid  = Temp      - id + dim
+    RHid = RhoH      - id + dim
+    YSid = FirstSpec - id + dim
+    YEid = LastSpec  - id + dim
+         
+    do_T  = (Tid  .GE. 1) .AND. (Tid  .LE. ncomp)
+    do_RH = (RHid .GE. 1) .AND. (RHid .LE. ncomp)
+    ys = MAX(YSid,1)
+    ye = MIN(YEid,ncomp)
+    do_Y = (ye - ys + 1) .GE. 1
+    !     
+    !     Do species, Temp, rhoH
+    !     
+    if ((idir.EQ.1) .AND. (lo(2) .LE. domlo(2))&
+         .AND. (do_T .OR. do_RH .OR. do_Y) ) then
+               
+       y = float(j)*dx(2)+problo(2)
+       j = lo(2)
+       do i = lo(1), hi(1)
+          
+          xl = float(i)*dx(1)+problo(1) 
+          xr = (float(i)+1.d0)*dx(1)+problo(1) 
+          xh = 0.5d0*(xl+xr)
+                  
+          if ( (getZone(xl,y).eq.BL_PIPEEND) .OR.&
+               (getZone(xh,y).eq.BL_PIPEEND) .OR.&
+               (getZone(xr,y).eq.BL_PIPEEND)  ) then
+                
+             !if (do_T)  diff(i,j,Tid ) = 0.d0
+             !if (do_RH) diff(i,j,RHid) = 0.d0
+             if (do_Y) then
+                do n=ys,ye
+                   diff(i,j,n) = 0.d0
+                enddo
+             endif
+                     
+          endif
+       end do
+    endif
+  end subroutine zero_visc
+  
+end module user_defined_fcts_2d_module
+
