8172e8a2c169d9c66297c3ff898dac276509ad50
Marc Day <MSDay@lbl.gov>
2019-07-31 09:17:29 -0700

    Adjust setup

---
+++ PeleLMruns/FlowGAN/probdata.F90
@@ -0,0 +1,19 @@
+#include <AMReX_REAL.H>
+
+
+module probdata_module
+
+  use mod_Fvar_def, only: maxspec
+
+  implicit none
+
+  REAL_T :: standoff, pert_scale, pertmag, blobz
+  REAL_T domnlo(3), domnhi(3)
+
+  logical :: bcinit = .false.
+  REAL_T :: u_bc, v_bc, w_bc
+  REAL_T :: Y_bc(0:maxspec-1), T_bc(1), h_bc(1), rho_bc(1)
+    
+contains
+
+end module probdata_module
