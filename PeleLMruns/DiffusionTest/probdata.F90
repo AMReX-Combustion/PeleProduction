505d779b7efdca77bc40b8434a9c48401bbe606a
Nick Wimer <nwimer@nrel.gov>
2019-09-10 15:46:33 -0600

    added DiffusionTest, blob of fuel to diffuse into ambient air

---
+++ PeleLMruns/DiffusionTest/probdata.F90
@@ -0,0 +1,27 @@
+#include <AMReX_REAL.H>
+
+
+module probdata_module
+
+  use network, only : nspecies
+
+  implicit none
+
+    ! from probdata.H
+    REAL_T :: T_in  
+    
+    ! from bc.H
+    
+    logical :: bcinit
+    integer, parameter :: nzones = 2
+    integer, parameter :: BL_FUEL = 1
+    integer, parameter :: BL_AIR = 2
+    
+    REAL_T :: u_bc(nzones), v_bc(nzones), w_bc(nzones)
+    REAL_T :: Y_bc(0:nspecies-1,nzones), T_bc(nzones), h_bc(nzones), rho_bc(nzones)
+      
+contains
+
+!subroutines here
+
+end module probdata_module
