1649304dfe18acd4dfe5a2007e7a2e1749f33f60
Marc Day <MSDay@lbl.gov>
2020-09-13 22:53:02 -0400

    Add weak scaling setups

---
+++ FlameSheetGPU/WeakScaling/2-Level/0032/batch_restart.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+#BSUB -P cmb138
+#BSUB -W 0:30
+#BSUB -nnodes 32
+#BSUB -alloc_flags gpumps
+#BSUB -J FlameSheet-rest-0032
+
+module load cmake gcc cuda
+jsrun -r 6 -a 1 -g 1 -c 7 -l GPU-CPU -d packed -b rs --smpiargs="-gpu" ./PeleLM3d.gnu.TPROF.MPI.CUDA.ex inputs.3d amr.restart=chk00000 amr.checkpoint_files_output=0 max_step=2 2>&1 | tee log_restart
+
+# n: # resources sets, where in each resource set:
+# a: # ranks
+# c: # cores
+# g: # gpus
---
+++ FlameSheetGPU/WeakScaling/2-Level/1024/batch_init.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+#BSUB -P cmb138
+#BSUB -W 0:30
+#BSUB -nnodes 1024
+#BSUB -alloc_flags gpumps
+#BSUB -J FlameSheet-init-1024
+
+module load cmake gcc cuda
+jsrun -r 6 -a 1 -g 1 -c 7 -l GPU-CPU -d packed -b rs --smpiargs="-gpu" ./PeleLM3d.gnu.TPROF.MPI.CUDA.ex inputs.3d max_step=0 2>&1 | tee log_init
+
+# n: # resources sets, where in each resource set:
+# a: # ranks
+# c: # cores
+# g: # gpus
