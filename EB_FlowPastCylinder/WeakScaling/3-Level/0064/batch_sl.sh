4259b74d1f526eb188ed64a150890bd3d618bcb1
Lucas Esclapez <lesclapez@login1.summit.olcf.ornl.gov>
2020-09-21 18:41:58 -0400

    Set up WeakScaling cases.

--- EB_FlowPastCylinder/WeakScaling/3-Level/0064/batch_sl.sh
+++ EB_FlowPastCylinder/WeakScaling/3-Level/0064/batch_sl.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 #BSUB -P cmb138
 #BSUB -W 0:30
-#BSUB -nnodes 1
+#BSUB -nnodes 64
 #BSUB -alloc_flags gpumps
-#BSUB -J FlowPastCyl-3L-0001
+#BSUB -J FlowPastCyl-3L-0064
 
 module load cmake gcc cuda
 jsrun -r 6 -a 1 -g 1 -c 7 -l GPU-CPU -d packed -b rs --smpiargs="-gpu" ./PeleLM3d.gnu.TPROF.MPI.CUDA.ex inputs.3d max_step=2 2>&1 | tee log_3L
