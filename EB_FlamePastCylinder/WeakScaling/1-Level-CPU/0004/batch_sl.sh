f3c89ef51dcdf3caca4d5dc968e1d8b41e20c39f
Lucas Esclapez <lesclapez@lbl.gov>
2020-09-23 00:30:25 -0700

    Add an EB_FlamePastCylinder single level weak scaling case.

---
+++ EB_FlamePastCylinder/WeakScaling/1-Level-CPU/0004/batch_sl.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+#BSUB -P cmb138
+#BSUB -W 0:30
+#BSUB -nnodes 4
+#BSUB -alloc_flags gpumps
+#BSUB -J FlowPastCyl-3L-0004
+
+module load cmake gcc cuda
+set -x
+export OMP_NUM_THREADS=7
+jsrun -p 24 -c 7 -r 6 -l cpu-cpu -d packed -b rs ./PeleLM3d.gnu.TPROF.MPI.OMP.ex inputs.3d max_step=2 2>&1 | tee log_3L
+
+# n: # resources sets, where in each resource set:
+# a: # ranks
+# c: # cores
+# g: # gpus
---
+++ EB_FlamePastCylinder/WeakScaling/1-Level/0004/batch_sl.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+#BSUB -P cmb138
+#BSUB -W 0:30
+#BSUB -nnodes 4
+#BSUB -alloc_flags gpumps
+#BSUB -J FlowPastCyl-3L-0004
+
+module load cmake gcc cuda
+jsrun -r 6 -a 1 -g 1 -c 7 -l GPU-CPU -d packed -b rs --smpiargs="-gpu" ./PeleLM3d.gnu.TPROF.MPI.CUDA.ex inputs.3d max_step=2 2>&1 | tee log_3L
+
+# n: # resources sets, where in each resource set:
+# a: # ranks
+# c: # cores
+# g: # gpus
