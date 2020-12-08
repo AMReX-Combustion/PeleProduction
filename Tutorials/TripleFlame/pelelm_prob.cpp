1d5797e0d517c4887183628776360d117385ba55
Lucas Esclapez <lesclapez@lbl.gov>
2020-12-07 21:16:25 -0800

    Adapt TripleFlame to the changes in ProbParm and ACParm.

--- Tutorials/TripleFlame/pelelm_prob.cpp
+++ Tutorials/TripleFlame/pelelm_prob.cpp
@@ -1,21 +1,5 @@
-
+#include <PeleLM.H>
 #include <pelelm_prob.H>
-#include <mechanism.h>
-
-namespace ProbParm
-{
-    AMREX_GPU_DEVICE_MANAGED  amrex::Real P_mean = 101325.0;
-    AMREX_GPU_DEVICE_MANAGED  amrex::Real splitx = - 0.0;
-    AMREX_GPU_DEVICE_MANAGED  amrex::Real midtanh = 0.001;
-    AMREX_GPU_DEVICE_MANAGED  amrex::Real widthtanh = 0.001;
-    AMREX_GPU_DEVICE_MANAGED  amrex::Real Zst = 0.055;
-    AMREX_GPU_DEVICE_MANAGED  amrex::Real T_in = 300.0;
-    AMREX_GPU_DEVICE_MANAGED  amrex::Real V_in = 0.4;
-
-    AMREX_GPU_DEVICE_MANAGED int fuelID = -1;
-    AMREX_GPU_DEVICE_MANAGED int bathID = -1;
-    AMREX_GPU_DEVICE_MANAGED int oxidID = -1;
-} // namespace ProbParm
 
 extern "C" {
     void amrex_probinit (const int* init,
@@ -26,19 +10,19 @@ extern "C" {
     {
         amrex::ParmParse pp("prob");
 
-        pp.query("P_mean", ProbParm::P_mean);
-        pp.query("Zst",    ProbParm::Zst);
-        pp.query("T_in",   ProbParm::T_in);
-        pp.query("V_in",   ProbParm::V_in);
+        pp.query("P_mean", PeleLM::prob_parm->P_mean);
+        pp.query("Zst",    PeleLM::prob_parm->Zst);
+        pp.query("T_in",   PeleLM::prob_parm->T_in);
+        pp.query("V_in",   PeleLM::prob_parm->V_in);
 
-        ProbParm::splitx = 0.5 * (problo[0] + probhi[0]);
-        ProbParm::midtanh = 0.6 * (problo[0] + probhi[0]);
-        ProbParm::widthtanh = 0.05 * (problo[0] + probhi[0]); 
+        PeleLM::prob_parm->splitx = 0.5 * (problo[0] + probhi[0]);
+        PeleLM::prob_parm->midtanh = 0.6 * (problo[0] + probhi[0]);
+        PeleLM::prob_parm->widthtanh = 0.05 * (problo[0] + probhi[0]); 
 
         // TODO: somewhat hard coded bath, fuel and oxid IDs 
         // should exist somewhere in PeleLM.
-        ProbParm::bathID = N2_ID;  
-        ProbParm::fuelID = CH4_ID;  
-        ProbParm::oxidID = O2_ID;  
+        PeleLM::prob_parm->bathID = N2_ID;  
+        PeleLM::prob_parm->fuelID = CH4_ID;  
+        PeleLM::prob_parm->oxidID = O2_ID;  
     }
 }
