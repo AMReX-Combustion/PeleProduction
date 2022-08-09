092fad7d582173c08e995b618a0d464f21fecda6
Landon Owen <ldowen@sandia.gov>
2022-08-09 11:27:24 -0700

    Added crusher test for open ticket

---
+++ PeleMPruns/PeleLMeX/laminar_soot/module_list.sh
@@ -0,0 +1,4 @@
+
+module load PrgEnv-amd rocm/5.1.0 craype-accel-amd-gfx90a cray-libsci/21.08.1.2
+export HIP_CLANG_INCLUDE_PATH=/gpfs/alpine/world-shared/csc380/LBPM-shared/llvm-internal/include
+export HIP_CLANG_PATH=/gpfs/alpine/world-shared/csc380/LBPM-shared/llvm-internal/bin
