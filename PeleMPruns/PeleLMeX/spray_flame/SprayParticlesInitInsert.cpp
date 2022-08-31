ff865453113b38704a36e977adf9f1913f28c249
Landon Owen <ldowen@sandia.gov>
2022-08-31 14:06:10 -0700

    Update PeleMP submodule and added a second liquid species to spray flame test

--- PeleMPruns/PeleLMeX/spray_flame/SprayParticlesInitInsert.cpp
+++ PeleMPruns/PeleLMeX/spray_flame/SprayParticlesInitInsert.cpp
@@ -13,15 +13,17 @@ SprayParticleContainer::injectParticles(
   ProbParm const& prob_parm)
 {
   amrex::ignore_unused(nstep, finest_level);
-  SprayJet* js = m_sprayJets[0].get();
+
   if (lev != 0) {
     return false;
   }
-  if (!js->jet_active(time)) {
-    return false;
+  for (int jet = 0; jet < m_sprayJets.size(); ++jet) {
+    SprayJet* js = m_sprayJets[jet].get();
+    if (!js->jet_active(time)) {
+      return false;
+    }
+    sprayInjection(js, dt, lev);
   }
-
-  sprayInjection(js, dt, lev);
 
   // Redistribute is done outside of this function
   return true;
@@ -32,9 +34,11 @@ SprayParticleContainer::InitSprayParticles(
   const bool init_parts, ProbParm const& prob_parm)
 {
   amrex::ignore_unused(prob_parm);
-  m_sprayJets.resize(1);
+  m_sprayJets.resize(2);
   std::string jet_name = "jet1";
   m_sprayJets[0] = std::make_unique<SprayJet>(jet_name, Geom(0));
+  jet_name = "jet2";
+  m_sprayJets[1] = std::make_unique<SprayJet>(jet_name, Geom(0));
   // Start without any particles
   m_injectVel = m_sprayJets[0]->jet_vel();
   return;
